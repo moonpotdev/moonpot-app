67bcf5f534d989b28fadfb784a4bab636786cfeb
Tumble17 <tumbledyer@protonmail.com>
2021-06-30 22:54:40 +0100

    fix: center vaults and break lines on vaults title

--- src/features/vault/vault.js
+++ src/features/vault/vault.js
@@ -65,59 +65,62 @@ const Vault = () => {
                 <Typography className={classes.title}>
                     <Trans i18nKey="vaultTitle" values={{name: item.name, apy: '147%', amount: '$587,338'}} />
                 </Typography>
-                <Box className={classes.potItem}>
-                    <Grid container>
-                        <Grid item xs={4}>
-                            <Typography className={classes.countdown}>1d 23h 15m</Typography>
-                            <Typography className={classes.subTitle}>{t('nextWeeklyDraw')}</Typography>
-                        </Grid>
-                        <Grid item xs={4}>
-                            <Box className={classes.potImage}>
-                                <img alt="Moonpot" src={require('../../images/pots/bitcoin.svg').default} />
-                            </Box>
-                        </Grid>
-                        <Grid item xs={4}>
-                            <Typography className={classes.apy}>10% APY</Typography>
-                            <Typography className={classes.subTitle} align={'right'} style={{textDecoration: 'underline'}}>{t('interestRate')}</Typography>
-                        </Grid>
-                        <Grid item xs={12}>
-                            <Typography className={classes.potUsd}><span>{t('win')}</span> $14,625 in {item.name}</Typography>
-                            <Typography className={classes.potCrypto}>USD {t('valueOf')} <span>16,400 Cake</span> {t('prize')}</Typography>
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField disabled={!wallet.address} className={classes.input} placeholder={t('enterCoinAmount', {coin: item.depositToken})} InputProps={{disableUnderline: true, classes: {root: classes.inputRoot}}} />
-                            {wallet.address ? (
-                                <Button className={classes.actionBtn} variant={'contained'} color="primary">{t('buttons.deposit')}</Button>
-                            ) : (
-                                <Button className={classes.actionBtn} variant={'contained'} color="primary">{t('buttons.connectWallet')}</Button>
-                            )}
-                        </Grid>
-                        <Grid item xs={12}>
-                            <Typography className={classes.timelockRemaining}>Two Weeks</Typography>
-                        </Grid>
-                        <Grid item xs={6}>
-                            <Link href={"a"} className={classes.link}>{t('fairplayTimelock')}</Link>
-                        </Grid>
-                        <Grid item xs={6} style={{textAlign: 'right'}}>
-                            <Link onClick={() => {setWithdrawOpen(!withdrawOpen)}} className={classes.link} style={{paddingRight: '15px'}}>{t('withdraw')} {withdrawOpen ? (<ArrowDropUp />) : (<ArrowDropDown />)}</Link>
-                        </Grid>
-                        <AnimateHeight duration={ 500 } height={ withdrawOpen ? 'auto' : 0 }>
+                <Grid container>
+                    <Box className={classes.potItem}>
+                        <Grid container>
+                            <Grid item xs={4}>
+                                <Typography className={classes.countdown}>1d 23h 15m</Typography>
+                                <Typography className={classes.subTitle}>{t('nextWeeklyDraw')}</Typography>
+                            </Grid>
+                            <Grid item xs={4}>
+                                <Box className={classes.potImage}>
+                                    <img alt="Moonpot" src={require('../../images/pots/bitcoin.svg').default} />
+                                </Box>
+                            </Grid>
+                            <Grid item xs={4}>
+                                <Typography className={classes.apy}>10% APY</Typography>
+                                <Typography className={classes.subTitle} align={'right'} style={{textDecoration: 'underline'}}>{t('interestRate')}</Typography>
+                            </Grid>
                             <Grid item xs={12}>
-                                <Typography className={classes.withdrawPenaltyWarning}>
-                                    <Trans i18nKey="vaultWithdrawPenaltyWarning" values={{amount: '2.52', coin: item.rewardToken}} />
-                                </Typography>
-                                <TextField disabled={!wallet.address} className={classes.input} placeholder="2.52 CAKE" InputProps={{disableUnderline: true, classes: {root: classes.inputRoot}}} />
+                                <Typography className={classes.potUsd}><span>{t('win')}</span> $14,625 in {item.name}</Typography>
+                                <Typography className={classes.potCrypto}>USD {t('valueOf')} <span>16,400 Cake</span> {t('prize')}</Typography>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField disabled={!wallet.address} className={classes.input} placeholder={t('enterCoinAmount', {coin: item.depositToken})} InputProps={{disableUnderline: true, classes: {root: classes.inputRoot}}} />
                                 {wallet.address ? (
-                                    <Button className={classes.actionBtn} variant={'contained'} color="primary">{t('buttons.withdraw')}</Button>
+                                    <Button className={classes.actionBtn} variant={'contained'} color="primary">{t('buttons.deposit')}</Button>
                                 ) : (
                                     <Button className={classes.actionBtn} variant={'contained'} color="primary">{t('buttons.connectWallet')}</Button>
                                 )}
-
                             </Grid>
-                        </AnimateHeight>
+                            <Grid item xs={12}>
+                                <Typography className={classes.timelockRemaining}>Two Weeks</Typography>
+                            </Grid>
+                            <Grid item xs={6}>
+                                <Link href={"a"} className={classes.link}>{t('fairplayTimelock')}</Link>
+                            </Grid>
+                            <Grid item xs={6} style={{textAlign: 'right'}}>
+                                <Link onClick={() => {setWithdrawOpen(!withdrawOpen)}} className={classes.link} style={{paddingRight: '15px'}}>{t('withdraw')} {withdrawOpen ? (<ArrowDropUp />) : (<ArrowDropDown />)}</Link>
+                            </Grid>
+                            <AnimateHeight duration={ 500 } height={ withdrawOpen ? 'auto' : 0 }>
+                                <Grid item xs={12}>
+                                    <Typography className={classes.withdrawPenaltyWarning}>
+                                        <Trans i18nKey="vaultWithdrawPenaltyWarning" values={{amount: '2.52', coin: item.rewardToken}} />
+                                    </Typography>
+                                    <TextField disabled={!wallet.address} className={classes.input} placeholder="2.52 CAKE" InputProps={{disableUnderline: true, classes: {root: classes.inputRoot}}} />
+                                    {wallet.address ? (
+                                        <Button className={classes.actionBtn} variant={'contained'} color="primary">{t('buttons.withdraw')}</Button>
+                                    ) : (
+                                        <Button className={classes.actionBtn} variant={'contained'} color="primary">{t('buttons.connectWallet')}</Button>
+                                    )}
 
-                    </Grid>
-                </Box>
+                                </Grid>
+                            </AnimateHeight>
+
+                        </Grid>
+                    </Box>
+                </Grid>
+                
             </Container>
             )}
         </div>
