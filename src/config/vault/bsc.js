8244e4ed496dbbc1312095fe7f3f34da614aad2c
Mo0o0d <yieldfarming1@gmail.com>
2021-07-09 10:03:12 +0100

    Update bsc.js

--- src/config/vault/bsc.js
+++ src/config/vault/bsc.js
@@ -7,7 +7,7 @@ export const pools = [
         tokenDecimals: 18,
         rewardToken: 'CAKE',
         rewardAddress: '0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82',
-        contractAddress: '0xC7686eB73aaABaD48081f1f1C976a48fA3ee915D',
+        contractAddress: '0x5818B516B5E57DBc9deBeE6011D0245c4fEAb8e9',
         pricePerShare: 1,
         vaultType: 'main',
         status: 'active',
