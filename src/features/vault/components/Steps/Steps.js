b5a80743e2dfe5d18083472cd2ab56f5781a6f96
Tumble17 <tumbledyer@protonmail.com>
2021-07-12 20:38:06 +0100

    feat: React CSS for deposit success

--- src/features/vault/components/Steps/Steps.js
+++ src/features/vault/components/Steps/Steps.js
@@ -1,8 +1,8 @@
-import {Backdrop, Box, Button, Fade, makeStyles, Modal, Typography} from "@material-ui/core";
+import {Backdrop, Grid, Box, Button, Link, Fade, makeStyles, Modal, Typography} from "@material-ui/core";
 import * as React from "react";
 import {byDecimals} from "../../../../helpers/format";
 import BigNumber from "bignumber.js";
-import {ArrowRight} from "@material-ui/icons";
+import {OpenInNew } from "@material-ui/icons";
 import {isEmpty} from "../../../../helpers/utils";
 import {Alert, AlertTitle} from "@material-ui/lab";
 import Loader from "../../../../components/loader";
@@ -29,12 +29,15 @@ const Steps = ({item, steps, handleClose}) => {
         >
             <Fade in={steps.modal}>
                 {steps.finished ? (
-                    <Box className={classes.modalContent}>
-                        <Box p={8} className={classes.finishedCard}>
+                    <Box className={classes.modalForeground}>
+                        <Grid container className={classes.modalText}>
+                            <Grid item xs={3}>
+                                <img alt="Moonpot Deposit" className={classes.confirmationImage} align={"left"} src={require('../../../../images/ziggy/depositSuccessful.svg').default} />
+                            </Grid>
                             {steps.items[steps.currentStep].step === 'deposit' ? (
                                 <React.Fragment>
-                                    <Typography variant={"h2"}>{byDecimals(new BigNumber(wallet.action.data.amount), item.tokenDecimals).toFixed(8)} {item.token}</Typography>
-                                    <Typography variant={"h2"}>deposit confirmed</Typography>
+                                    <Typography className={classes.stepsTitleText}>Deposit Successful!</Typography>
+                                    <Typography className={classes.successfulDepositAmountText}>You have successfully deposited {byDecimals(new BigNumber(wallet.action.data.amount), item.tokenDecimals).toFixed(8)} {item.token}</Typography>
                                 </React.Fragment>
                             ) : (
                                 <React.Fragment>
@@ -42,39 +45,46 @@ const Steps = ({item, steps, handleClose}) => {
                                     <Typography variant={"h2"}>withdraw confirmed</Typography>
                                 </React.Fragment>
                             )}
-                            <Typography>Funds are on the way</Typography>
-                            <Box mt={1} textAlign={"center"}>
-                                <Button variant={"outlined"} color={"primary"} href={wallet.explorer[item.network] + '/tx/' + wallet.action.data.receipt.transactionHash} target="_blank">View on Explorer</Button> <Button variant={"outlined"} color={"primary"} onClick={handleClose}>Close Dialog</Button>
+                            <Box className={classes.viewMyMoonpots} textAlign={"center"}>
+                                <Button href={"/my-moonpots"}>View My Moonpots</Button>
                             </Box>
-                        </Box>
-                        <Box mt={2} textAlign={"center"}>
-                            <Button variant={"outlined"} color={"primary"} href={"/my-moonpots"}>Go to my portfolio <ArrowRight /></Button>
-                        </Box>
+                            <Box textAlign={"center"}>
+                                <Link className={classes.blockExplorerLink} variant={"outlined"} href={wallet.explorer[item.network] + '/tx/' + wallet.action.data.receipt.transactionHash} target="_blank">See deposit on Block Explorer <OpenInNew /></Link> 
+                            </Box>
+                        </Grid>
                     </Box>
                 ) : (
-                    <Box className={classes.modalContent}>
-                        <Typography id="transition-modal-title" variant={"h2"}>{steps.currentStep} / {steps.items.length} transactions<br />confirmed</Typography>
-                        <Typography id="transition-modal-description" variant={"body2"}>{!isEmpty(steps.items[steps.currentStep]) ? steps.items[steps.currentStep].message : ''}</Typography>
-                        {wallet.action && wallet.action.result === 'error' ? (
-                            <Alert severity={"error"}>
-                                <AlertTitle>Error</AlertTitle>
-                                <Typography>{wallet.action.data.error}</Typography>
-                                <Box textAlign={"center"} mt={2}>
-                                    <Button variant={"outlined"} color={"primary"} onClick={handleClose}>Close</Button>
-                                </Box>
-                            </Alert>
-                        ) : ''}
-                        {wallet.action && wallet.action.result === 'success_pending' ? (
-                            <Alert severity={"info"}>
-                                <AlertTitle>Confirmation Pending</AlertTitle>
-                                <Typography>Waiting for network to confirm transaction...</Typography>
-                                <Box textAlign={"center"}><Loader /></Box>
-                                <Box textAlign={"center"} mt={2}>
-                                    <Button variant={"outlined"} color={"primary"} href={wallet.explorer[item.network] + '/tx/' + wallet.action.data.hash} target="_blank">View on Explorer</Button>
-                                </Box>
-                            </Alert>
-                        ) : ''}
-                    </Box>
+                    <React.Fragment>
+                        <Box className={classes.modalForeground}>
+                            <Grid container className={classes.modalText}>
+                                <Grid item xs={3}>
+                                    <img alt="Moonpot Deposit" className={classes.confirmationImage} align={"left"} src={require('../../../../images/ziggy/deposit.svg').default} />
+                                </Grid>
+                                <Grid item xs={10}>
+                                    <Typography id="transition-modal-title" className={classes.transactionConfirmations}>{steps.currentStep} / {steps.items.length} Transactions Confirmed</Typography>
+                                </Grid>
+                                <Typography id="transition-modal-description">{!isEmpty(steps.items[steps.currentStep]) ? steps.items[steps.currentStep].message : ''}</Typography>
+                                
+                                {wallet.action && wallet.action.result === 'error' ? (
+                                    <Alert severity={"error"}>
+                                        <AlertTitle>Error</AlertTitle>
+                                        <Typography>{wallet.action.data.error}</Typography>
+                                    </Alert>
+                                ) : ''}
+                                {wallet.action && wallet.action.result === 'success_pending' ? (
+                                    <Alert severity={"info"}>
+                                        <AlertTitle>Confirmation Pending</AlertTitle>
+                                        <Typography>Waiting for network to confirm transaction...</Typography>
+                                        <Box textAlign={"center"}><Loader /></Box>
+                                        <Box textAlign={"center"} mt={2}>
+                                            <Button variant={"outlined"} color={"primary"} href={wallet.explorer[item.network] + '/tx/' + wallet.action.data.hash} target="_blank">See deposit on Block Explorer</Button>
+                                        </Box>
+                                    </Alert>
+                                ) : ''}
+                            </Grid>
+                        </Box>
+                        <Button variant={"outlined"} onClick={handleClose}>Close</Button>
+                    </React.Fragment>
                 )}
             </Fade>
         </Modal>
