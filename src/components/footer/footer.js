80d6f8aba5ca01cd025eff642376ee2edb2666dd
Nicolas Woodcock <nicolas@socialsquared.ca>
2021-07-29 19:03:46 -0400

    #44 Follow up commit
    -Switched from PNG to SVG for footer images to improve clarity

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -16,35 +16,15 @@ const Footer = () => {
                     <Button href={"https://moonpot.com/alpha"} className={classes.footerBtn} variant={'outlined'} size={'small'}>{t('buttons.moonpotAlpha')}</Button>
                 </Grid>
                 <Grid className={classes.footerIcons} item xs={12}>
-                    <Link href={"https://github.com/moonpotdev"}><img alt="Github" srcSet={
-                        require('../../images/footer/github@4x.png').default + ' 4x',
-                        require('../../images/footer/github@3x.png').default + ' 3x', 
-                        require('../../images/footer/github@2x.png').default + ' 2x',
-                        require('../../images/footer/github@1x.png').default + ' 1x'} />
+                    <Link href={"https://github.com/moonpotdev"}><img alt="Github" src={require('../../images/footer/github.svg').default} className={classes.footerImage}/>
                     </Link>
-                    <Link href={"https://t.me/moonpotdotcom"}><img alt="Telegram" srcSet={
-                        require('../../images/footer/telegram@4x.png').default + ' 4x',
-                        require('../../images/footer/telegram@3x.png').default + ' 3x', 
-                        require('../../images/footer/telegram@2x.png').default + ' 2x',
-                        require('../../images/footer/telegram@1x.png').default + ' 1x'} />
+                    <Link href={"https://t.me/moonpotdotcom"}><img alt="Telegram" src={require('../../images/footer/telegram.svg').default} className={classes.footerImage} />
                     </Link>
-                    <Link href={"https://discord.gg/8YquFwfw3N"}><img alt="Discord" srcSet={
-                        require('../../images/footer/discord@4x.png').default + ' 4x',
-                        require('../../images/footer/discord@3x.png').default + ' 3x', 
-                        require('../../images/footer/discord@2x.png').default + ' 2x',
-                        require('../../images/footer/discord@1x.png').default + ' 1x'} />
+                    <Link href={"https://discord.gg/8YquFwfw3N"}><img alt="Discord" src={require('../../images/footer/discord.svg').default} className={classes.footerImage} />
                     </Link>
-                    <Link href={"https://www.youtube.com/channel/UCCwZh5FBKusmzmT5Q3Yisdg"}><img alt="Youtube" srcSet={
-                        require('../../images/footer/youtube@4x.png').default + ' 4x',
-                        require('../../images/footer/youtube@3x.png').default + ' 3x', 
-                        require('../../images/footer/youtube@2x.png').default + ' 2x',
-                        require('../../images/footer/youtube@1x.png').default + ' 1x'} />
+                    <Link href={"https://www.youtube.com/channel/UCCwZh5FBKusmzmT5Q3Yisdg"}><img alt="Youtube" src={require('../../images/footer/youtube.svg').default} className={classes.footerImage} />
                     </Link>
-                    <Link href={"https://twitter.com/moonpotdotcom"}><img alt="Twitter" srcSet={
-                        require('../../images/footer/twitter@4x.png').default + ' 4x',
-                        require('../../images/footer/twitter@3x.png').default + ' 3x', 
-                        require('../../images/footer/twitter@2x.png').default + ' 2x',
-                        require('../../images/footer/twitter@1x.png').default + ' 1x'} />
+                    <Link href={"https://twitter.com/moonpotdotcom"}><img alt="Twitter" src={require('../../images/footer/twitter.svg').default} className={classes.footerImage} />
                     </Link>
                 </Grid>
             </Grid>
