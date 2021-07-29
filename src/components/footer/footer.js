b8fd2c622452457b757095d7b9b588cff2fb1c00
Nicolas Woodcock <nicolas@socialsquared.ca>
2021-07-29 14:00:36 -0400

    feat/enhanced footer images clean up
    -Correctly use images from src
    -Remove unused images in public

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -16,36 +16,36 @@ const Footer = () => {
                     <Button href={"https://moonpot.com/alpha"} className={classes.footerBtn} variant={'outlined'} size={'small'}>{t('buttons.moonpotAlpha')}</Button>
                 </Grid>
                 <Grid className={classes.footerIcons} item xs={12}>
-                    <Link href={"https://github.com/moonpotdev"}><img alt="Github" srcSet={`
-                                                            images/footer/github@4x.png 4x,
-                                                            images/footer/github@3x.png 3x,
-                                                            images/footer/github@2x.png 2x,
-                                                            images/footer/github@1x.png 1x,
-                                                        `} /></Link>
-                    <Link href={"https://t.me/moonpotdotcom"}><img alt="Telegram" srcSet={`
-                                                            images/footer/telegram@4x.png 4x,
-                                                            images/footer/telegram@3x.png 3x,
-                                                            images/footer/telegram@2x.png 2x,
-                                                            images/footer/telegram@1x.png 1x,
-                                                        `} /></Link>
-                    <Link href={"https://discord.gg/8YquFwfw3N"}><img alt="Discord" srcSet={`
-                                                            images/footer/discord@4x.png 4x,
-                                                            images/footer/discord@3x.png 3x,
-                                                            images/footer/discord@2x.png 2x,
-                                                            images/footer/discord@1x.png 1x,
-                                                        `} /></Link>
-                    <Link href={"https://www.youtube.com/channel/UCCwZh5FBKusmzmT5Q3Yisdg"}><img alt="Youtube" srcSet={`
-                                                            images/footer/youtube@4x.png 4x,
-                                                            images/footer/youtube@3x.png 3x,
-                                                            images/footer/youtube@2x.png 2x,
-                                                            images/footer/youtube@1x.png 1x,
-                                                        `} /></Link>
-                    <Link href={"https://twitter.com/moonpotdotcom"}><img alt="Twitter" srcSet={`
-                                                            images/footer/twitter@4x.png 4x,
-                                                            images/footer/twitter@3x.png 3x,
-                                                            images/footer/twitter@2x.png 2x,
-                                                            images/footer/twitter@1x.png 1x,
-                                                        `} /></Link>
+                    <Link href={"https://github.com/moonpotdev"}><img alt="Github" srcSet={
+                        require('../../images/footer/github@4x.png').default + ' 4x',
+                        require('../../images/footer/github@3x.png').default + ' 3x', 
+                        require('../../images/footer/github@2x.png').default + ' 2x',
+                        require('../../images/footer/github@1x.png').default + ' 1x'} />
+                    </Link>
+                    <Link href={"https://t.me/moonpotdotcom"}><img alt="Telegram" srcSet={
+                        require('../../images/footer/telegram@4x.png').default + ' 4x',
+                        require('../../images/footer/telegram@3x.png').default + ' 3x', 
+                        require('../../images/footer/telegram@2x.png').default + ' 2x',
+                        require('../../images/footer/telegram@1x.png').default + ' 1x'} />
+                    </Link>
+                    <Link href={"https://discord.gg/8YquFwfw3N"}><img alt="Discord" srcSet={
+                        require('../../images/footer/discord@4x.png').default + ' 4x',
+                        require('../../images/footer/discord@3x.png').default + ' 3x', 
+                        require('../../images/footer/discord@2x.png').default + ' 2x',
+                        require('../../images/footer/discord@1x.png').default + ' 1x'} />
+                    </Link>
+                    <Link href={"https://www.youtube.com/channel/UCCwZh5FBKusmzmT5Q3Yisdg"}><img alt="Youtube" srcSet={
+                        require('../../images/footer/youtube@4x.png').default + ' 4x',
+                        require('../../images/footer/youtube@3x.png').default + ' 3x', 
+                        require('../../images/footer/youtube@2x.png').default + ' 2x',
+                        require('../../images/footer/youtube@1x.png').default + ' 1x'} />
+                    </Link>
+                    <Link href={"https://twitter.com/moonpotdotcom"}><img alt="Twitter" srcSet={
+                        require('../../images/footer/twitter@4x.png').default + ' 4x',
+                        require('../../images/footer/twitter@3x.png').default + ' 3x', 
+                        require('../../images/footer/twitter@2x.png').default + ' 2x',
+                        require('../../images/footer/twitter@1x.png').default + ' 1x'} />
+                    </Link>
                 </Grid>
             </Grid>
         </React.Fragment>
