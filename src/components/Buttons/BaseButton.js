505d2a62279d9ae2338e7f984733568cf04293ce
ReflectiveChimp <55021052+ReflectiveChimp@users.noreply.github.com>
2021-07-31 03:17:45 +0100

    404 page

---
+++ src/components/Buttons/BaseButton.js
@@ -0,0 +1,29 @@
+import {Button, withStyles} from '@material-ui/core';
+import {useHistory} from 'react-router-dom';
+import {forwardRef, useCallback} from 'react';
+
+const RoutedButton = forwardRef(function ({ref, to, ...rest}) {
+	const history = useHistory();
+	const handleClick = useCallback(() => {
+		if (to) {
+			history.push(to);
+		}
+	}, [history, to]);
+
+	return <Button ref={ref} onClick={to ? handleClick : null} {...rest}/>
+});
+
+export const BaseButton = withStyles({
+	root: {
+		borderRadius: '8px',
+		fontWeight: 700,
+		fontSize: '15px',
+		lineHeight: '24px',
+		letterSpacing: '0.2px',
+		display: 'block',
+		padding: 12,
+		height: 'auto',
+		textAlign: 'center',
+	}
+})(RoutedButton);
+
---
+++ src/components/Cards/Cards.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {useStyles} from './styles';
+import classNames from 'classnames';
+import {Typography} from '@material-ui/core';
+
+function variantClass(classes, prefix, variant) {
+	const key = prefix + variant[0].toUpperCase() + variant.substr(1);
+	return key in classes ? classes[key] : classes[prefix + 'White'];
+}
+
+export function Cards({variant, className, children, ...rest}) {
+	const classes = useStyles();
+
+	return <div className={classNames(classes.cards, className)} {...rest}>{children}</div>;
+}
+
+export function Card({variant = 'white', className, children, ...rest}) {
+	const classes = useStyles();
+
+	return <div className={classNames(classes.card, variantClass(classes, 'card', variant), className)} {...rest}>{children}</div>;
+}
+
+export function CardTitle({variant = 'white', className, children, ...rest}) {
+	const classes = useStyles();
+
+	return <Typography variant="h2" className={classNames(classes.title, variantClass(classes, 'title', variant), className)} {...rest}>{children}</Typography>;
+}
