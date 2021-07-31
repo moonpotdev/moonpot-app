9908803254d92df1749d2c0d04a6a5d9497f6818
ReflectiveChimp <55021052+ReflectiveChimp@users.noreply.github.com>
2021-08-01 00:22:51 +0100

    Redo header

--- src/app.js
+++ src/app.js
@@ -1,184 +1,63 @@
-import React, {useContext, useState} from 'react';
-import {HashRouter, Route, Switch, useHistory} from 'react-router-dom';
+import React, {Suspense} from 'react';
+import {HashRouter, Route, Switch} from 'react-router-dom';
+import {useDispatch} from 'react-redux';
 import appTheme from './appTheme.js';
-import Header from './components/header';
-import Footer from './components/footer';
-import {Box, CssBaseline, Grid, Link, makeStyles, ThemeProvider} from '@material-ui/core';
-import MenuRoundedIcon from '@material-ui/icons/MenuRounded';
-import {useDispatch, useSelector} from 'react-redux';
+import {CssBaseline, ThemeProvider} from '@material-ui/core';
 import reduxActions from './features/redux/actions';
-import {slide as Menu} from 'react-burger-menu';
-import {useTranslation} from 'react-i18next';
-import WalletContainer from './components/header/components/WalletContainer';
-import {createHashHistory} from 'history';
-import Media from 'react-media';
-import {burgerMenuStyles, styles} from './styles.js';
+import {RouteLoading} from './components/RouteLoading';
 import {PageNotFound} from './PageNotFound';
-import {RouteLoading} from './components/RouteLoading/RouteLoading';
+import {Header} from './components/Header';
+import Footer from './components/footer';
 
 const Home = React.lazy(() => import(`./features/home`));
 const Vault = React.lazy(() => import(`./features/vault`));
 const Dashboard = React.lazy(() => import(`./features/dashboard`));
 const Dao = React.lazy(() => import(`./features/dao`));
 
-const Context = React.createContext();
-
-const Provider = (props) => {
-    const [menuOpenState, setMenuOpenState] = useState(false)
-
-    return (
-        <Context.Provider value={{
-            isMenuOpen: menuOpenState,
-            toggleMenu: () => setMenuOpenState(!menuOpenState),
-            stateChangeHandler: (newState) => {
-                setMenuOpenState(newState.isOpen);
-
-                let nextLogoState = ( newState.isOpen ) ? "none" : "";
-                let stateTimeout = ( newState.isOpen ) ? 0 : 250;
-
-                setTimeout(function() {
-                    // element does not exist on resize render
-                    const logo = document.getElementById("logo");
-                    if (logo) {
-                        logo.style.display = nextLogoState;
-                    }
-
-                }, stateTimeout);
-            }
-          }}>
-            {props.children}
-          </Context.Provider>
-    )
-};
-
-const Navigation = () => {
-    const ctx = useContext(Context)
-
-    const {t} = useTranslation();
-    const history = useHistory();
-    const walletReducer = useSelector(state => state.walletReducer);
-
-    const useStyles = makeStyles(styles);
-    const classes = useStyles();
-
-    return (
-        <React.Fragment>
-                <Box id="logo" onClick={() => {history.push('/')}}>
-                    <img alt="Moonpot" className={classes.moonpotImage} src={require('./images/moonpot-dot-com.png').default}/>
-                </Box>
-            <Menu
-                customBurgerIcon={ <MenuRoundedIcon/> }
-                isOpen={ctx.isMenuOpen}
-                onStateChange={(state) => ctx.stateChangeHandler(state)}
-                disableAutoFocus
-                styles={ burgerMenuStyles }
-            >
-                <Grid
-                container
-                direction="column"
-                justifyContent="space-evenly"
-                spacing={3}
-                >
-                    <Grid item xs={10} style={{height: '15%'}}>
-
-                    </Grid>
-                    <Grid item xs={10} align={"left"}>
-                        <Link
-                        className={classes.mobileNavItem}
-                        onClick={() => {
-                            history.push('/');
-                            ctx.toggleMenu();
-                        }}>
-                            {t('buttons.moonpots')}
-                        </Link>
-                    </Grid>
-                    <Grid item xs={10} align={"left"}>
-                        <Link className={classes.mobileNavItem} onClick={() => {
-                            history.push('/my-moonpots');
-                            ctx.toggleMenu();
-                        }}>
-                            {t('buttons.myPots')}
-                        </Link>
-                    </Grid>
-                    <Grid item xs={10} align={"left"}>
-                        <Link className={classes.mobileNavItem} onClick={() => {
-                            history.push('/ido');
-                            ctx.toggleMenu();
-                        }}>
-                            {t('buttons.ido')}
-                        </Link>
-                    </Grid>
-                    <Grid item xs={10} align={"left"}>
-                        <Link className={classes.mobileNavItem} href={"https://docs.moonpot.com"} onClick={() => {
-                            ctx.toggleMenu();
-                        }}>
-                            {t('buttons.docs')}
-                        </Link>
-                    </Grid>
-                    <Grid item xs={12} className={classes.mobileNavWallet} align={"center"} onClick={() => {
-                            (walletReducer.address ? void 0 : ctx.toggleMenu())
-                    }}>
-                        <WalletContainer />
-                    </Grid>
-                </Grid>
-            </Menu>
-        </React.Fragment>
-
-
-    )
-  }
+function Pages() {
+	return <Suspense fallback={<RouteLoading/>}>
+		<Switch>
+			<Route exact path="/" key={Date.now()}>
+				<Home/>
+			</Route>
+			<Route strict sensitive exact path="/pot/:id">
+				<Vault/>
+			</Route>
+			<Route strict sensitive exact path="/my-moonpots/:status?">
+				<Dashboard/>
+			</Route>
+			<Route strict sensitive exact path="/ido">
+				<Dao/>
+			</Route>
+			<Route>
+				<PageNotFound/>
+			</Route>
+		</Switch>
+	</Suspense>;
+}
 
 export default function App() {
-    const dispatch = useDispatch();
-    const theme = appTheme();
+	const dispatch = useDispatch();
+	const theme = appTheme();
 
-    React.useEffect(() => {
-        const initiate = async () => {
-            await dispatch(reduxActions.prices.fetchPrices());
-        }
-        return initiate();
-    }, [dispatch]);
+	React.useEffect(() => {
+		dispatch(reduxActions.prices.fetchPrices());
+	}, [dispatch]);
 
-    React.useEffect(() => {
-        dispatch(reduxActions.wallet.createWeb3Modal());
-    }, [dispatch]);
+	React.useEffect(() => {
+		dispatch(reduxActions.wallet.createWeb3Modal());
+	}, [dispatch]);
 
-    return (
-        <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <HashRouter history={createHashHistory()}>
-                <Provider>
-                    <Media query="(max-width: 1100px)">
-                        {matches =>
-                            matches ? (
-                                <Navigation />
-                            ) : (
-                                <Header />
-                            )
-                        }
-                    </Media>
-                    <React.Suspense fallback={<RouteLoading/>}>
-                        <Switch>
-                            <Route exact path="/" key={Date.now()}>
-                                <Home />
-                            </Route>
-                            <Route strict sensitive exact path="/pot/:id">
-                                <Vault />
-                            </Route>
-                            <Route strict sensitive exact path="/my-moonpots/:status?">
-                                <Dashboard />
-                            </Route>
-                            <Route strict sensitive exact path="/ido">
-                                <Dao />
-                            </Route>
-                            <Route>
-                                <PageNotFound />
-                            </Route>
-                        </Switch>
-                    </React.Suspense>
-                    <Footer />
-                </Provider>
-            </HashRouter>
-        </ThemeProvider>
-    );
+	return (
+		<ThemeProvider theme={theme}>
+			<CssBaseline/>
+			<HashRouter>
+				<Header/>
+				<div id="site-inner">
+					<Pages/>
+					<Footer/>
+				</div>
+			</HashRouter>
+		</ThemeProvider>
+	);
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,125 @@
+import React, {useCallback, useState} from 'react';
+import {styles} from './styles';
+import {useTranslation} from 'react-i18next';
+import {useHistory, useRouteMatch} from 'react-router';
+import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
+import {Drawer, IconButton, makeStyles, useMediaQuery} from '@material-ui/core';
+import WalletConnector from './components/WalletConnector';
+import MoonpotDotCom from '../../images/moonpot-white-single-line.svg';
+import classNames from 'classnames';
+
+const useStyles = makeStyles(styles);
+
+const links = [
+	{
+		label: 'buttons.moonpots',
+		href: '/',
+		match: {exact: true},
+	},
+	{
+		label: 'buttons.myPots',
+		href: '/my-moonpots',
+	},
+	{
+		label: 'buttons.ido',
+		href: '/ido',
+	},
+	{
+		label: 'buttons.docs',
+		href: 'https://docs.moonpot.com',
+		external: true,
+	},
+];
+
+function MenuLink({external, href, label, match, onClick, ...rest}) {
+	const {t} = useTranslation();
+	const history = useHistory();
+	const active = useRouteMatch({
+		path: href,
+		...(match || {})
+	});
+
+	const handleClick = useCallback((e) => {
+		if (!external) {
+			e.preventDefault();
+			history.push(href);
+		}
+
+		if (onClick) {
+			onClick();
+		}
+	}, [external, href, history, onClick]);
+
+	const props = {
+		href: href,
+	};
+
+	if (external) {
+		props.target = '_blank';
+		props.rel = 'noreferrer';
+	}
+
+	return <a {...rest} {...props} onClick={handleClick} data-active={active}>{t(label)}</a>;
+}
+
+function Sidebar() {
+	const classes = useStyles();
+	const [isOpen, setOpen] = useState(false);
+	const handleOpen = useCallback(() => setOpen(true), [setOpen]);
+	const handleClose = useCallback(() => setOpen(false), [setOpen]);
+
+	return <>
+		<IconButton className={classes.toggleSidebar} color="inherit" aria-label="Show Sidebar" onClick={handleOpen}>
+			<MenuIcon/>
+		</IconButton>
+		<Drawer anchor="right" open={isOpen} onClose={handleClose} classes={{paper: classes.sidebar}}>
+			<div className={classes.sidebarTop}>
+				<IconButton className={classes.toggleSidebar} color="inherit" aria-label="Hide Sidebar"
+							onClick={handleClose}>
+					<CloseIcon/>
+				</IconButton>
+			</div>
+			<nav className={classes.sidebarNav}>
+				{links.map(link => <MenuLink key={link.href} {...link} onClick={handleClose}
+											 className={classes.sidebarItem}/>)}
+			</nav>
+			<div className={classes.sidebarBottom}>
+				<WalletConnector onConnect={handleClose} fullWidth={true}/>
+			</div>
+		</Drawer>
+	</>;
+}
+
+function Nav() {
+	const classes = useStyles();
+
+	return <nav className={classes.nav}>
+		{links.map(link => <MenuLink key={link.href} {...link} className={classes.navItem}/>)}
+	</nav>;
+}
+
+export function Header() {
+	const classes = useStyles();
+	const history = useHistory();
+	const showSidebar = useMediaQuery('(max-width:779.99px)');
+
+	const handleHome = useCallback((e) => {
+		e.preventDefault();
+		history.push('/');
+	}, [history]);
+
+	return <div className={classes.bar}>
+		<div className={classes.barInner}>
+			<div className={classes.barItem}>
+				<a href="/" onClick={handleHome} className={classes.logoLink}>
+					<img src={MoonpotDotCom} alt="Moonpot.com" className={classes.logo} width="157" height="36"/>
+				</a>
+			</div>
+			{showSidebar ? null : <div className={classNames(classes.barItem, classes.pushRight)}><Nav/></div>}
+			<div className={classNames(classes.barItem, {[classes.pushRight]: showSidebar})}>
+				{showSidebar ? <Sidebar/> : <WalletConnector variant="small"/>}
+			</div>
+		</div>
+	</div>;
+}
