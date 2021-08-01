116bde5cdcfaf3f2650b39adacf828634fcfe2c5
Nicolas Woodcock <nicolas@socialsquared.ca>
2021-07-31 23:29:19 -0400

    feat: play with more cake label
    Closes #50

--- src/features/home/home.js
+++ src/features/home/home.js
@@ -30,10 +30,11 @@ const Home = () => {
     const { t } = useTranslation();
     const location = useLocation();
     const dispatch = useDispatch();
-    const {vault, prices} = useSelector(state => ({
+    const walletReducer = useSelector(state => state.walletReducer);
+    const {vault, prices, balance} = useSelector(state => ({
         vault: state.vaultReducer,
         prices: state.pricesReducer,
-
+        balance: state.balanceReducer,
     }));
 
     const history = useHistory();
@@ -171,7 +172,7 @@ const Home = () => {
                                                 <Divider className={classes.divider} style={{marginBottom:'20px'}}/>
                                             </Grid>
                                             <Grid item xs={12}>
-                                                <Button className={classes.play} variant={'contained'} onClick={() => {history.push('/pot/' + (item.id))}}>{t('buttons.playWith')} {item.token}</Button>
+                                                <Button className={classes.play} variant={'contained'} onClick={() => {history.push('/pot/' + (item.id))}}>{t('buttons.playWith')} {balance.tokens[item.rewardToken].balance && walletReducer.address > 0 ? t('buttons.more') : ""} {item.token}</Button>
                                             </Grid>
                                             <Grid item xs={8}>
                                                 <Typography className={classes.oddsPerDeposit}>{t('oddsPerDeposit', {odds: investmentOdds(item.totalStakedUsd, BigNumber(1000), item.numberOfWinners), amount: '$1000'})}</Typography>
