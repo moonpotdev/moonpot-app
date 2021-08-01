9800e9237fb3df5a258f8be6e58234d1a1fd55aa
ReflectiveChimp <55021052+ReflectiveChimp@users.noreply.github.com>
2021-08-01 16:32:24 +0100

    Remove unused import

--- src/features/home/components/Filter/Filter.js
+++ src/features/home/components/Filter/Filter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Button, Grid } from '@material-ui/core';
+import { Button, Grid } from '@material-ui/core';
 import { useTranslation } from 'react-i18next';
 
 const Filter = ({ sortConfig, setSortConfig, defaultFilter }) => {
