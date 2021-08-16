002f7017eb0ce94f73ef1cd218691684d690f4b8
Nicolas Woodcock <nicolas@socialsquared.ca>
2021-08-16 19:23:33 -0400

    Update to use new components

--- src/components/Modal/layouts/wrongChainModal.js
+++ src/components/Modal/layouts/wrongChainModal.js
@@ -1,47 +1,48 @@
 import React from 'react';
-import {Card, CardTitle} from '../../Cards/Cards';
-import {ButtonPurpleWhite} from '../../Buttons/ButtonPurpleWhite';
+import { Card, CardTitle } from '../../Cards/Cards';
+import { PrimaryButton } from '../../Buttons/PrimaryButton';
 import { BaseButton } from '../../Buttons/BaseButton';
-import {Typography, makeStyles} from '@material-ui/core';
+import { Typography, makeStyles } from '@material-ui/core';
 import reduxActions from '../../../features/redux/actions';
-import {useDispatch} from 'react-redux';
-import {networkSetup} from '../../../config/config';
-import {useTranslation} from 'react-i18next';
-import styles from "./styles"
-
+import { useDispatch } from 'react-redux';
+import { networkSetup } from '../../../config/config';
+import { useTranslation } from 'react-i18next';
+import styles from './styles';
 
 const useStyles = makeStyles(styles);
 
 const WrongChainModal = () => {
+  const dispatch = useDispatch();
+  const { t } = useTranslation();
+  const classes = useStyles();
 
-    const dispatch = useDispatch();
-    const { t } = useTranslation();
-    const classes = useStyles();
+  function closeModal() {
+    dispatch(reduxActions.modal.hideModal());
+  }
 
-    function closeModal() {
-        dispatch(reduxActions.modal.hideModal());
-    }
+  function switchToNetwork(networdIdentifier) {
+    networkSetup(networdIdentifier)
+      .catch(e => {
+        console.error(e);
+        dispatch(reduxActions.modal.showUnableToSwitchChainModal());
+      })
+      .then(dispatch(reduxActions.modal.hideModal()));
+  }
 
-    function switchToNetwork(networdIdentifier) {
-        networkSetup(networdIdentifier).catch((e) => {
-            console.error(e);
-            dispatch(reduxActions.modal.showUnableToSwitchChainModal());
-          }).then(
-            dispatch(reduxActions.modal.hideModal())
-          );
-    }
-
-    return (
-      <React.Fragment>
-        <Card className={classes.card}>
-			    <CardTitle className={classes.title}>{t('modals.unsupportedNetworkDetected')}</CardTitle>
-			    <Typography className={classes.text}>{t('modals.switchToBSCNetwork')}</Typography>
-          <ButtonPurpleWhite onClick={ () => switchToNetwork('bsc')} className={classes.switchButton}>{t('modals.switchToBSC')}</ButtonPurpleWhite>
-			  </Card>
-        <BaseButton onClick={ () => closeModal()} className={classes.dismissButton}>{t('modals.cancel')}</BaseButton>
-      </React.Fragment>
-			  
-    )
-}
+  return (
+    <React.Fragment>
+      <Card variant="white" className={classes.card}>
+        <CardTitle className={classes.title}>{t('modals.unsupportedNetworkDetected')}</CardTitle>
+        <Typography className={classes.text}>{t('modals.switchToBSCNetwork')}</Typography>
+        <PrimaryButton onClick={() => switchToNetwork('bsc')} className={classes.switchButton}>
+          {t('modals.switchToBSC')}
+        </PrimaryButton>
+      </Card>
+      <BaseButton onClick={() => closeModal()} className={classes.dismissButton}>
+        {t('modals.cancel')}
+      </BaseButton>
+    </React.Fragment>
+  );
+};
 
 export default WrongChainModal;
