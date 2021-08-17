22682c2f3bb1f641b456bd7dbc527d38bdb7fb47
Nicolas Woodcock <nicolas@socialsquared.ca>
2021-08-17 18:58:10 -0400

    Fix

--- src/components/Modal/modal.js
+++ src/components/Modal/modal.js
@@ -1,32 +1,26 @@
 import React from 'react';
 import Modal from '@material-ui/core/Modal';
-import {useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
 import WrongChainModal from './layouts/wrongChainModal';
 import UnableToSwitchChainModal from './layouts/unableToSwitchChainModal';
 
 function renderSwitch(type) {
-    switch(type) {
-        case 'WRONG_CHAIN_MODAL':
-            return <WrongChainModal />;
-        case 'UNABLE_TO_SWITCH_CHAIN_MODAL':
-            return <UnableToSwitchChainModal />;
-        default:
-            return "";
-    }
+  //Check payload for modal layout
+  switch (type) {
+    case 'WRONG_CHAIN_MODAL':
+      return <WrongChainModal />;
+    case 'UNABLE_TO_SWITCH_CHAIN_MODAL':
+      return <UnableToSwitchChainModal />;
+    default:
+      return '';
+  }
 }
 
 const ModalPopup = () => {
+  let isOpen = useSelector(state => state.modalReducer.isOpen);
+  let modalType = useSelector(state => state.modalReducer.modalType);
 
-    let isOpen = useSelector(state => state.modalReducer.isOpen);
-    let modalType = useSelector(state => state.modalReducer.modalType);
-
-    return (
-        <Modal open={isOpen}>
-            {renderSwitch(modalType)}
-        </Modal>
-    )
-    
-}
+  return <Modal open={isOpen}>{renderSwitch(modalType)}</Modal>;
+};
 
 export default ModalPopup;
-
