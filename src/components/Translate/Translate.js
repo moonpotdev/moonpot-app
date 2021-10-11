d855da163d15bd29423360920af26d4e3bb2bb2a
ReflectiveChimp <55021052+ReflectiveChimp@users.noreply.github.com>
2021-10-11 19:48:31 +0100

    Update SidePotExplainer to use one translation string / work with other languages Update 'Learn more' link in SidePotExplainer to point directly to article

--- src/components/Translate/Translate.js
+++ src/components/Translate/Translate.js
@@ -1,7 +1,7 @@
 import React, { memo } from 'react';
 import { Trans, useTranslation } from 'react-i18next';
 
-export const Translate = memo(function ({ i18nKey, values }) {
+export const Translate = memo(function (props) {
   const { t } = useTranslation();
-  return <Trans t={t} i18nKey={i18nKey} values={values} />;
+  return <Trans t={t} {...props} />;
 });
