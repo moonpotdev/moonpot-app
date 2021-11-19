6ae1f2b0d9266815b13d3c900f08dabbbe82837b
ReflectiveChimp <55021052+ReflectiveChimp@users.noreply.github.com>
2021-11-19 19:47:35 +0000

    Use live beefy api

--- src/features/redux/actions/prices.js
+++ src/features/redux/actions/prices.js
@@ -99,12 +99,9 @@ async function updatePrices() {
   };
 
   try {
-    const request = await axios.get(
-      'https://beefy-api.herokuapp.com/prices?_=' + getApiCacheBuster(),
-      {
-        timeout: 2000,
-      }
-    );
+    const request = await axios.get('https://api.beefy.finance/prices?_=' + getApiCacheBuster(), {
+      timeout: 2000,
+    });
     return request.status === 200 ? request.data : retry();
   } catch (err) {
     console.log('error fetchPrices()', err);
@@ -120,12 +117,9 @@ async function updateLPPrices() {
   };
 
   try {
-    const request = await axios.get(
-      'https://beefy-api.herokuapp.com/lps?_=' + getApiCacheBuster(),
-      {
-        timeout: 2000,
-      }
-    );
+    const request = await axios.get('https://api.beefy.finance/lps?_=' + getApiCacheBuster(), {
+      timeout: 2000,
+    });
     return request.status === 200 ? request.data : retry();
   } catch (err) {
     console.log('error fetchPrices()', err);
@@ -141,7 +135,7 @@ async function updateApy() {
   };
   try {
     const request = await axios.get(
-      'https://beefy-api.herokuapp.com/apy/breakdown?_=' + getApiCacheBuster(),
+      'https://api.beefy.finance/apy/breakdown?_=' + getApiCacheBuster(),
       { timeout: 2000 }
     );
     return request.status === 200 ? request.data : retry();
