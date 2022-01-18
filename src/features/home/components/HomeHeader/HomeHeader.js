f16f60826acfb3acc8dbcd5b897cb8f0de2e974d
Radioactive <nicolas@socialsquared.ca>
2022-01-18 16:02:13 -0500

    Connect Header Data
    Connected:
    -Total Prizes
    -Cadets
    -Current Prizes
    -Next Featured Draw Countdown

--- src/features/home/components/HomeHeader/HomeHeader.js
+++ src/features/home/components/HomeHeader/HomeHeader.js
@@ -1,61 +1,56 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Translate } from '../../../../components/Translate';
-import { Logo } from '../../../../components/Pot/Pot';
-import { PrimaryButton } from '../../../../components/Buttons/PrimaryButton';
 import { Grid, makeStyles } from '@material-ui/core';
 import styles from './styles';
 import coins from '../../../../images/ziggy/coins.png';
+import { useSelector } from 'react-redux';
+import { usePots } from '../../../../helpers/hooks';
+import HeaderCountdown from './components/HeaderCountdown';
+import { useTotalPrizeValue } from '../../../winners/apollo/total';
 
 const useStyles = makeStyles(styles);
 
 const HomeHeader = () => {
   const classes = useStyles();
 
+  //Next Featured Draw Countdown Data
+  //get all pots
+  const pots = usePots();
+  //convert to array and sort out filtered pots
+  const featuredPots = Object.entries(pots).filter(pot => {
+    return pot[1].featured === true;
+  });
+  //get the pot with the soonest draw
+  const nextFeaturedPot = featuredPots.reduce(function (prev, current) {
+    return prev[1].expiresAt < current[1].expiresAt ? prev : current;
+  });
+
+  //Holders Data
+  const cadets = useSelector(state => state.holdersReducer.totalHolders || 0);
+  const cadetsFormatted = cadets.toLocaleString(undefined, {
+    maximumFractionDigits: 0,
+  });
+
+  //Available Prizes Data
+  const totalPrizesAvailable = useSelector(
+    state =>
+      state.vaultReducer.projectedTotalPrizesAvailable || state.vaultReducer.totalPrizesAvailable
+  );
+  const totalPrizesAvailableFormatted = useMemo(() => {
+    return totalPrizesAvailable.toNumber().toLocaleString(undefined, {
+      maximumFractionDigits: 0,
+    });
+  }, [totalPrizesAvailable]);
+
+  //Total Prizes Data
+  const { total } = useTotalPrizeValue();
+  const totalFormatted = total.toLocaleString(undefined, {
+    maximumFractionDigits: 0,
+  });
+
   return (
     <div className={classes.container}>
-      <div className={classes.nextDrawCard}>
-        {/* Top of card */}
-        <div className={classes.nextDrawInner}>
-          {/* Prize + Countdown */}
-          <div>
-            <div className={classes.nextDrawPrizeText}>
-              <Translate
-                i18nKey="header.winPrizeInToken"
-                values={{ prize: '$69,420', token: 'CAKE' }}
-              />
-            </div>
-            <div className={classes.nextDrawTimeContainer}>
-              <div>
-                <div className={classes.nextDrawTimeValue}>1</div>
-                <div className={classes.nextDrawTimeLabel}>
-                  <Translate i18nKey="header.hours" />
-                </div>
-              </div>
-              <div className={classes.nextDrawTimeSeparator}>:</div>
-              <div>
-                <div className={classes.nextDrawTimeValue}>23</div>
-                <div className={classes.nextDrawTimeLabel}>
-                  <Translate i18nKey="header.mins" />
-                </div>
-              </div>
-              <div className={classes.nextDrawTimeSeparator}>:</div>
-              <div>
-                <div className={classes.nextDrawTimeValue}>45</div>
-                <div className={classes.nextDrawTimeLabel}>
-                  <Translate i18nKey="header.secs" />
-                </div>
-              </div>
-            </div>
-          </div>
-          {/* Pot Logo */}
-          <div className={classes.nextDrawLogoContainer}>
-            <Logo icon={'cake'} sponsorToken={'pots'} />
-          </div>
-        </div>
-        <PrimaryButton to={`/pot/cake`} variant={'purpleHeader'} fullWidth={true}>
-          <Translate i18nKey={'pot.playWith'} values={{ token: 'CAKE' }} />
-        </PrimaryButton>
-      </div>
+      <HeaderCountdown pot={nextFeaturedPot} />
       <div className={classes.statsCard}>
         <Grid container style={{ height: '100%' }}>
           <Grid item xs={6} className={classes.statsVertCenterOuter}>
@@ -63,7 +58,7 @@ const HomeHeader = () => {
               <div className={classes.statsCardLabel}>
                 <Translate i18nKey="header.totalWon" />
               </div>
-              <div className={classes.statsCardValue}>$4,824,200</div>
+              <div className={classes.statsCardValue}>${totalFormatted}</div>
             </div>
           </Grid>
           <Grid item xs={6} className={classes.statsVertCenterOuter}>
@@ -79,7 +74,7 @@ const HomeHeader = () => {
               <div className={classes.statsCardLabel}>
                 <Translate i18nKey="header.currentPrizes" />
               </div>
-              <div className={classes.statsCardValue}>$395,575</div>
+              <div className={classes.statsCardValue}>${totalPrizesAvailableFormatted}</div>
             </div>
           </Grid>
           <Grid item xs={6} className={classes.statsVertCenterOuter}>
@@ -87,7 +82,7 @@ const HomeHeader = () => {
               <div className={classes.statsCardLabel}>
                 <Translate i18nKey="header.cadets" />
               </div>
-              <div className={classes.statsCardValue}>23,456</div>
+              <div className={classes.statsCardValue}>{cadetsFormatted}</div>
             </div>
           </Grid>
         </Grid>
---
+++ src/features/home/components/HomeHeader/components/HeaderCountdown.js
@@ -0,0 +1,94 @@
+import React, { useState, useEffect } from 'react';
+import { Translate } from '../../../../../components/Translate';
+import { Logo } from '../../../../../components/Pot/Pot';
+import { PrimaryButton } from '../../../../../components/Buttons/PrimaryButton';
+import { makeStyles } from '@material-ui/core';
+import { formatTimeLeft } from '../../../../../helpers/format';
+import styles from '../styles';
+
+const useStyles = makeStyles(styles);
+
+const HeaderCountdown = ({ pot }) => {
+  const classes = useStyles();
+  const potData = pot[1];
+
+  //Set time for countdown calculation
+  const [time, setTime] = useState(Date.now());
+  useEffect(() => {
+    const id = setInterval(() => setTime(Date.now()), 1000);
+    return () => clearInterval(id);
+  }, [setTime]);
+
+  //Calculate time untill draw
+  const expiresAt = potData.expiresAt * 1000;
+  const timeLeft = Math.max(0, expiresAt - time);
+  const timeToDraw = formatTimeLeft(
+    timeLeft,
+    {
+      resolution: 'seconds',
+      dropZero: false,
+    },
+    true
+  );
+
+  return (
+    <div className={classes.nextDrawCard}>
+      {/* Top of card */}
+      <div className={classes.nextDrawInner}>
+        {/* Prize + Countdown */}
+        <div>
+          <div className={classes.nextDrawPrizeText}>
+            {/* TODO: Add multiple token support and support for pots with both token and NFT prizes */}
+            {potData.nftPrizeOnly && !potData.nfts ? (
+              //Token Only Prize
+              <Translate
+                i18nKey="header.winPrizeInToken"
+                values={{ prize: potData.totalPrizeUSD, token: potData.token }}
+              />
+            ) : potData.nftPrizeOnly ? (
+              //NFT Only Prize
+              <Translate i18nKey="header.winNFT" values={{ nft: potData.nfts[0].name }} />
+            ) : null}
+          </div>
+          <div className={classes.nextDrawTimeContainer}>
+            <div>
+              <div className={classes.nextDrawTimeValue}>
+                {timeToDraw[1] === 'NaN' ? '00' : timeToDraw[1]}
+              </div>
+              <div className={classes.nextDrawTimeLabel}>
+                <Translate i18nKey="header.hours" />
+              </div>
+            </div>
+            <div className={classes.nextDrawTimeSeparator}>:</div>
+            <div>
+              <div className={classes.nextDrawTimeValue}>
+                {timeToDraw[2] === 'NaN' ? '00' : timeToDraw[2]}
+              </div>
+              <div className={classes.nextDrawTimeLabel}>
+                <Translate i18nKey="header.mins" />
+              </div>
+            </div>
+            <div className={classes.nextDrawTimeSeparator}>:</div>
+            <div>
+              <div className={classes.nextDrawTimeValue}>
+                {timeToDraw[3] === 'NaN' ? '00' : timeToDraw[3]}
+              </div>
+              <div className={classes.nextDrawTimeLabel}>
+                <Translate i18nKey="header.secs" />
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Pot Logo */}
+        <div className={classes.nextDrawLogoContainer}>
+          <Logo icon={potData.icon || potData.id} sponsorToken={potData.sponsorToken} />
+        </div>
+      </div>
+      <PrimaryButton to={`/pot/` + potData.id} variant={'purpleHeader'} fullWidth={true}>
+        <Translate i18nKey={'pot.playWith'} values={{ token: potData.token }} />
+      </PrimaryButton>
+    </div>
+  );
+};
+
+export default HeaderCountdown;
